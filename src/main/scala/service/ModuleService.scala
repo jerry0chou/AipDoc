bc2f79011a61b41c54f1b8516eb52b2144072600
jerry0chu <1052757325@qq.com>
2019-11-23 22:21:09 +0800

    alter getModApi Sql with Left join sql

--- src/main/scala/service/ModuleService.scala
+++ src/main/scala/service/ModuleService.scala
@@ -4,7 +4,6 @@ import model.Tables._
 import slick.jdbc.SQLiteProfile.api._
 import utils.Store.{ModApiList, ModuleVar, SimpleApi}
 import utils.result._
-
 import scala.collection.mutable.ArrayBuffer
 
 object ModuleService
@@ -15,24 +14,22 @@ object ModuleService
 
     def getModApi(projId: Int) =
     {
-       val query=sql"""
-                SELECT module.mod_id ,module.mod_name,api.api_id,api.api_name
-                from module,api
-                 WHERE module.mod_id = api.mod_id and module.proj_id=${projId}
-                 ORDER BY module.mod_id ASC
-               """.as[(Int,String,Int,String)]
-
-        db.run(query).map(ele=>{
-            val map=ele.groupBy(e => (e._1, e._2)).mapValues(e => e.map(e => (e._3, e._4)))
-            val res=ArrayBuffer[ModApiList]()
+        val query = sql"""
+               SELECT module.mod_id ,module.mod_name,api.api_id,api.api_name
+               from module LEFT JOIN api ON module.mod_id = api.mod_id
+               WHERE module.proj_id=${projId}  ORDER BY module.mod_id ASC
+               """.as[(Int, String, Int, String)]
+        db.run(query).map(ele => {
+            val map = ele.groupBy(e => (e._1, e._2)).mapValues(e => e.map(e => (e._3, e._4)))
+            val res = ArrayBuffer[ModApiList]()
             for ((k, v) <- map) {
-                val ab=ArrayBuffer[SimpleApi]()
-                v.map{e=>
-                    ab+=SimpleApi(e._1,e._2)
+                val ab = ArrayBuffer[SimpleApi]()
+                v.filterNot(_._2==null).map { e =>
+                    ab += SimpleApi(e._1, e._2)
                 }
-                res+=ModApiList(k._1,k._2,ab)
+                res += ModApiList(k._1, k._2, ab)
             }
-            success(res,"getModApi successfully")
+            success(res, "getModApi successfully")
         })
     }
 
