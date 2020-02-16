6b79898a085dbfcf9bfba60fcb9a50f27ba21618
jerry0chu <1052757325@qq.com>
2020-02-16 17:12:19 +0800

    make a war package and then upload into my server

--- src/main/scala/controller/IndexServlet.scala
+++ src/main/scala/controller/IndexServlet.scala
@@ -4,6 +4,7 @@ import org.scalatra.{FutureSupport, ScalatraServlet}
 import slick.jdbc.MySQLProfile.api._
 import org.scalatra._
 import service.ProjectService.queryApiInfo
+import utils.Config.{staticHead}
 import utils.Store.ApiInfo
 
 class IndexServlet extends ScalatraServlet with FutureSupport
@@ -11,7 +12,6 @@ class IndexServlet extends ScalatraServlet with FutureSupport
     protected implicit def executor = scala.concurrent.ExecutionContext.Implicits.global
 
     get("/") {
-
         views.html.index()
     }
 }
